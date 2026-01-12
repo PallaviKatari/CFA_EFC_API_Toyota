94775604ffb44658ef7798ae539462a4c3cdbaa3
PallaviKatari <pallavikatari@gmail.com>
2026-01-12 14:51:30 +0530

    Commit_StoredProcedure_12.1.26

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,47 @@
+﻿using CFA_EFC_API.Data;
+using CFA_EFC_API.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+namespace CFA_EFC_API.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public StudentsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // READ
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var students = await _context
+                .Set<StudentDto>()
+                .FromSqlRaw("EXEC dbo.GetStudents") // Call the procedure
+                .AsNoTracking() 
+                .ToListAsync();
+
+            return Ok(students);
+        }
+
+        // WRITE
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] StudentDto model)
+        {
+            await _context.Database.ExecuteSqlRawAsync(
+                "EXEC dbo.InsertStudent @Name, @Age",
+                new SqlParameter("@Name", model.Name),
+                new SqlParameter("@Age", model.Age)
+            );
+
+            return Ok("Student inserted");
+        }
+    }
+}
--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,9 +1,10 @@
-﻿using CFA_EFC_API.Models;
+﻿using CFA_EFC_API.DTO;
+using CFA_EFC_API.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace CFA_EFC_API.Data
 {
-    public class AppDbContext: DbContext
+    public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options)
         : base(options) { }
@@ -11,5 +12,10 @@ namespace CFA_EFC_API.Data
         //Product - Database - Singular form
         //Products - .NET - Plural form
         public DbSet<Product> Product => Set<Product>();
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<StudentDto>().HasNoKey();
+        }
     }
+
 }
